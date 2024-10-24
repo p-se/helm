79235995f9e238958790607bf7e986f8cf5372c7
Patrick Seidensal <pseidensal@suse.com>
2024-10-24 16:04:54 +0200

    Use three way merge for unstructured objects
    Based on work of wxdao <waxiadao@gmail.com> in the same issue,
    supersedes #9938. Basically just adds tests.
    
    closes #9937
    
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -26,6 +26,7 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/discovery"
@@ -93,6 +94,11 @@ type Configuration struct {
 
 	// Capabilities describes the capabilities of the Kubernetes cluster.
 	Capabilities *chartutil.Capabilities
+
+	// ThreeWayMergeForUnstructured controls whether to use three way merge
+	// patch for unstructured objects (custom resource, custom definitions,
+	// etc).
+	ThreeWayMergeForUnstructured bool
 
 	Log func(string, ...interface{})
 }
@@ -373,6 +379,7 @@ func (cfg *Configuration) recordRelease(r *release.Release) {
 func (cfg *Configuration) Init(getter genericclioptions.RESTClientGetter, namespace, helmDriver string, log DebugLog) error {
 	kc := kube.New(getter)
 	kc.Log = log
+	kc.ThreeWayMergeForUnstructured = cfg.ThreeWayMergeForUnstructured
 
 	lazyClient := &lazyClient{
 		namespace: namespace,
